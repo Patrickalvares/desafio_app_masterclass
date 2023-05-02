3451be4cd241a53a0e723dac24a0645c7a235d98
Notebook Patrick <patrickalvares@outlook.com>
2023-05-02 11:08:50 -0300

    Componentização dos containers

---
+++ lib/components/customCard.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String imagePath;
+  final String description;
+  final int exerciseCount;
+
+  CustomCard({
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    required this.exerciseCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 13, top: 8, bottom: 8),
+      child: Container(
+        width: 400,
+        height: 220,
+        decoration: BoxDecoration(
+          color: colorScheme.background,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10, left: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 43,
+                        width: 43,
+                        decoration: BoxDecoration(
+                          color: colorScheme.primary,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: Image.asset(imagePath),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 13),
+                        child: Text(title,
+                            style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                color: colorScheme.secondary)),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 17),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            'Exercicios:',
+                            style: GoogleFonts.montserrat(
+                                color: colorScheme.onSurface, fontSize: 12),
+                          ),
+                        ),
+                        Text(exerciseCount.toString(),
+                            style: GoogleFonts.poppins(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                color: colorScheme.secondary)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                description,
+                style: GoogleFonts.montserrat(
+                    color: colorScheme.onSurface, fontSize: 14),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(bottom: 21.5, right: 14, left: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: Image.asset('lib/assets/awesome-github.png'),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.36),
+                          child: Text(
+                            'Acessar código fonte',
+                            style: GoogleFonts.montserrat(
+                                color: colorScheme.secondary, fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 34.5,
+                      width: 119,
+                      decoration: BoxDecoration(
+                        color: colorScheme.primary,
+                        borderRadius: BorderRadius.circular(48),
+                      ),
+                      child: Center(
+                        child: Text('Ver mais',
+                            style: GoogleFonts.poppins(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                color: colorScheme.secondary)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:desafio_app_masterclass/components/customCard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -14,174 +15,86 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              primary: const Color(0xFF055AA3),
+              secondary: const Color(0xFFEDF4F8),
+              onSurface: const Color(0xFF51565A),
+              background: const Color(0xFF172026),
+            ),
       ),
       home: Scaffold(
           backgroundColor: const Color(0xFF121517),
-          body: Column(
-            children: [
-              const SizedBox(height: 48),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 21, right: 4),
-                        child: SizedBox(
-                          child: Image.asset('lib/assets/logo.png'),
+          body: ListView(children: [
+            Column(
+              children: [
+                const SizedBox(height: 48),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 21, right: 4),
+                          child: SizedBox(
+                            child: Image.asset('lib/assets/logo.png'),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Atividades',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: const Color(0xFFEDF4F8))),
+                            Text('Flutterando Masterclass',
+                                style: GoogleFonts.poppins(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600,
+                                    color: const Color(0xFFEDF4F8))),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 26.55),
+                      child: SizedBox(
+                        width: 21.45,
+                        height: 24,
+                        child: Image.asset(
+                          'lib/assets/awesome-moon.png',
+                          color: const Color(0xFFEDF4F8),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Atividades',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: const Color(0xFFEDF4F8))),
-                          Text('Flutterando Masterclass',
-                              style: GoogleFonts.poppins(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w600,
-                                  color: const Color(0xFFEDF4F8))),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 26.55),
-                    child: SizedBox(
-                      width: 21.45,
-                      height: 24,
-                      child: Image.asset(
-                        'lib/assets/awesome-moon.png',
-                        color: const Color(0xFFEDF4F8),
-                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15, right: 13, top: 8, bottom: 8),
-                child: Container(
-                    width: 400,
-                    height: 220,
-                    decoration: BoxDecoration(
-                        color: const Color(0xFF172026),
-                        borderRadius: BorderRadius.circular(30)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 10, left: 12),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 43,
-                                    width: 43,
-                                    decoration: BoxDecoration(
-                                        color: const Color(0xFF055AA3),
-                                        borderRadius:
-                                            BorderRadius.circular(100)),
-                                    child: Image.asset(
-                                        'lib/assets/awesome-running.png'),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 13),
-                                    child: Text('Animações',
-                                        style: GoogleFonts.poppins(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600,
-                                            color: const Color(0xFFEDF4F8))),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 17),
-                                child: Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(right: 10),
-                                      child: Text(
-                                        'Exercicios:',
-                                        style: GoogleFonts.montserrat(
-                                            color: const Color(0xFF51565A),
-                                            fontSize: 12),
-                                      ),
-                                    ),
-                                    Text('4',
-                                        style: GoogleFonts.poppins(
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w600,
-                                            color: const Color(0xFFEDF4F8))),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                          Text(
-                            'Estudos sobre animações implícitas e controladas, contendo 4 exercícios e 2 estudos',
-                            style: GoogleFonts.montserrat(
-                                color: const Color(0xFF51565A), fontSize: 14),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                bottom: 21.5, right: 14, left: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    SizedBox(
-                                      height: 20,
-                                      width: 20,
-                                      child: Image.asset(
-                                          'lib/assets/awesome-github.png'),
-                                    ),
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 4.36),
-                                      child: Text(
-                                        'Acessar código fonte',
-                                        style: GoogleFonts.montserrat(
-                                            color: const Color(0xFFEDF4F8),
-                                            fontSize: 12),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Container(
-                                  height: 34.5,
-                                  width: 119,
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xFF055AA3),
-                                    borderRadius: BorderRadius.circular(48),
-                                  ),
-                                  child: Center(
-                                    child: Text('Ver mais',
-                                        style: GoogleFonts.poppins(
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w600,
-                                            color: const Color(0xFFEDF4F8))),
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    )),
-              )
-            ],
-          )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                CustomCard(
+                  title: 'Animações',
+                  imagePath: 'lib/assets/awesome-running.png',
+                  description:
+                      'Estudos sobre animações implícitas e controladas, contendo 4 exercícios e 2 estudos',
+                  exerciseCount: 4,
+                ),
+                CustomCard(
+                  title: 'Leitura de Mockup',
+                  imagePath: 'lib/assets/Icon awesome-glasses.png',
+                  description:
+                      'Aplicação da técnica de leitura de mockup, contendo 2 exercícios',
+                  exerciseCount: 2,
+                ),
+                CustomCard(
+                  title: 'Playground',
+                  imagePath: 'lib/assets/Icon material-toys.png',
+                  description: 'Ambiente destinado a testes e estudos em geral',
+                  exerciseCount: 3,
+                ),
+              ],
+            ),
+          ])),
     );
   }
 }
